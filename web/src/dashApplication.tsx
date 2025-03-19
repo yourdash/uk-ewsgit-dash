ba10a9bb78f681ec4189762c8bc8e0fef8ddba90
ewsgit <ewsgit@duck.com>
2025-03-19 23:16:21 +0000

    back to somewhat functional

--- web/src/dashApplication.tsx
+++ web/src/dashApplication.tsx
@@ -17,7 +17,7 @@ import {
   UKHeading,
   UKIconButton,
   UKIcons,
-  UKSeparator,
+  UKSeparator, UKText,
   UKTextInput
 } from "@yourdash/uikit";
 
@@ -140,174 +140,190 @@ const DashApplication: React.FC = () => {
   if (!dashboard) return <>LOADING...</>;
 
   return (<div
-          className={styles.page}
-          style={dashboard.background.type === "color" ? { backgroundColor: dashboard.background.value } : dashboard.background.type === "linearGradient" ? { backgroundImage: `linear-gradient(${dashboard.background.value})` } : { backgroundImage: `url(${tun.baseUrl + "/uk-ewsgit-dash/backgroundImage"})` }}
-      >
-        <UKFlex
-            className={styles.header}
-            direction={"column"}
-            centerHorizontally
-            centerVertically
-            style={{
-              // @ts-ignore
-              "--opacity": `${dashboard.header.background.opacity}`,
-              backdropFilter: `blur(${4 * dashboard.header.background.blur}rem)`,
-              paddingTop: headerPaddingLevelToValue(dashboard.header.size),
-              paddingBottom: headerPaddingLevelToValue(dashboard.header.size),
-            }}
-        >
-          <UKHeading
-              style={{
-                fontFamily: dashboard.header.font.family,
-                fontWeight: dashboard.header.font.weight,
-                fontSize: `${dashboard.header.font.size}rem`,
-              }}
-              level={1}
-              text={dashboard.header.welcomeMessage.replace(
-                  `%username%`, `${dashboard.user.forename} ${dashboard.user.surname}`)}
-          />
-        </UKFlex>
-        {isWidgetEditMode ? (<>
-              <UKCard
-                  containerClassName={styles.editModeCardContainer}
-                  className={styles.editModeCard}
-                  actions={<>
-                    <UKSeparator direction={"column"}/>
-                    <UKFlex direction={"row"}>
-                      <UKButton
-                          text={"Cancel"}
-                          onClick={() => {
-                            setIsWidgetEditMode(false);
-                          }}
-                      />
-                      <UKButton
-                          text={"Submit"}
-                          onClick={() => {
-                            return 0;
-                          }}
-                      />
-                    </UKFlex>
-                  </>}
-              >
-                <UKHeading
-                    text={"Content"}
-                    style={{ textAlign: "start" }}
-                />
-                <UKSeparator direction={"column"}/>
-                <UKFlex direction={"column"} className={styles.parameterContainer}>
-                  <UKContainer>
-                    <UKHeading
-                        className={styles.heading}
-                        style={{ textAlign: "start" }}
-                        text={"Header font size"}
-                        level={4}
-                    />
-                    <UKTextInput
-                        className={styles.input}
-                        getValue={() => {}}
-                        placeholder={"4rem"}
-                        accessibleName={"Header font size"}
-                    />
-                  </UKContainer>
-                  <UKContainer>
-                    <UKHeading
-                        className={styles.heading}
-                        style={{ textAlign: "start" }}
-                        text={"Header font family"}
-                        level={4}
-                    />
-                    <UKTextInput
-                        className={styles.input}
-                        getValue={() => {}}
-                        placeholder={"Inter"}
-                        accessibleName={"Header font family"}
-                    />
-                  </UKContainer>
-                  <UKContainer>
-                    <UKHeading
-                        className={styles.heading}
-                        style={{ textAlign: "start" }}
-                        text={"Header font weight"}
-                        level={4}
-                    />
-                    <UKTextInput
-                        className={styles.input}
-                        getValue={() => {}}
-                        placeholder={"900"}
-                        accessibleName={"Header font weight"}
-                    />
-                  </UKContainer>
-                  <UKContainer>
-                    <UKHeading
-                        className={styles.heading}
-                        style={{ textAlign: "start" }}
-                        text={"Header background opacity (%)"}
-                        level={4}
-                    />
-                    <UKTextInput
-                        className={styles.input}
-                        getValue={() => {}}
-                        placeholder={"0.75 (75%)"}
-                        accessibleName={"Header background opacity"}
-                    />
-                  </UKContainer>
-                  <UKContainer>
-                    <UKHeading
-                        className={styles.heading}
-                        style={{ textAlign: "start" }}
-                        text={"Header background blur (%)"}
-                        level={4}
-                    />
-                    <UKTextInput
-                        className={styles.input}
-                        getValue={() => {}}
-                        placeholder={"0.75 (75%)"}
-                        accessibleName={"Header background blur"}
-                    />
-                  </UKContainer>
-                </UKFlex>
-              </UKCard>
-            </>) : (<UKFlex
-                direction={"row"}
-                className={styles.widgetsCarouselContainer}
-            >
-              <UKCarousel
-                  className={styles.carousel}
-                  items={[
-                    {
-                      element: (<div className={styles.widgetGrid}>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                          </div>), id: "page1",
-                    }, {
-                      element: (<div className={styles.widgetGrid}>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                            <UKCard>Item</UKCard>
-                          </div>), id: "page2",
-                    },
-                  ]}
+      className={styles.page}
+      style={dashboard.background.type === "color" ? { backgroundColor: dashboard.background.value } : dashboard.background.type === "linearGradient" ? { backgroundImage: `linear-gradient(${dashboard.background.value})` } : { backgroundImage: `url(${tun.baseUrl + "/uk-ewsgit-dash/backgroundImage"})` }}
+  >
+    <UKFlex
+        className={styles.header}
+        direction={"column"}
+        centerHorizontally
+        centerVertically
+        style={{
+          // @ts-ignore
+          "--opacity": `${dashboard.header.background.opacity}`,
+          backdropFilter: `blur(${4 * dashboard.header.background.blur}rem)`,
+          paddingTop: headerPaddingLevelToValue(dashboard.header.size),
+          paddingBottom: headerPaddingLevelToValue(dashboard.header.size),
+        }}
+    >
+      <UKHeading
+          style={{
+            fontFamily: dashboard.header.font.family,
+            fontWeight: dashboard.header.font.weight,
+            fontSize: `${dashboard.header.font.size}rem`,
+          }}
+          level={1}
+          text={dashboard.header.welcomeMessage.replace(
+              `%username%`, `${dashboard.user.forename} ${dashboard.user.surname}`)}
+      />
+    </UKFlex>
+    {isWidgetEditMode ? (<>
+      <UKCard
+          containerClassName={styles.editModeCardContainer}
+          className={styles.editModeCard}
+          actions={<>
+            <UKSeparator direction={"column"}/>
+            <UKFlex direction={"row"}>
+              <UKButton
+                  text={"Cancel"}
+                  onClick={() => {
+                    setIsWidgetEditMode(false);
+                  }}
               />
-            </UKFlex>)}
-      </div>);
+              <UKButton
+                  text={"Submit"}
+                  onClick={() => {
+                    return 0;
+                  }}
+              />
+            </UKFlex>
+          </>}
+      >
+        <UKHeading
+            text={"Content"}
+            style={{ textAlign: "start" }}
+        />
+        <UKSeparator direction={"column"}/>
+        <UKFlex direction={"column"} className={styles.parameterContainer}>
+          <UKContainer>
+            <UKHeading
+                className={styles.heading}
+                style={{ textAlign: "start" }}
+                text={"Header font size"}
+                level={4}
+            />
+            <UKTextInput
+                className={styles.input}
+                getValue={() => {}}
+                placeholder={"4rem"}
+                accessibleName={"Header font size"}
+            />
+          </UKContainer>
+          <UKContainer>
+            <UKHeading
+                className={styles.heading}
+                style={{ textAlign: "start" }}
+                text={"Header font family"}
+                level={4}
+            />
+            <UKTextInput
+                className={styles.input}
+                getValue={() => {}}
+                placeholder={"Inter"}
+                accessibleName={"Header font family"}
+            />
+          </UKContainer>
+          <UKContainer>
+            <UKHeading
+                className={styles.heading}
+                style={{ textAlign: "start" }}
+                text={"Header font weight"}
+                level={4}
+            />
+            <UKTextInput
+                className={styles.input}
+                getValue={() => {}}
+                placeholder={"900"}
+                accessibleName={"Header font weight"}
+            />
+          </UKContainer>
+          <UKContainer>
+            <UKHeading
+                className={styles.heading}
+                style={{ textAlign: "start" }}
+                text={"Header background opacity (%)"}
+                level={4}
+            />
+            <UKTextInput
+                className={styles.input}
+                getValue={() => {}}
+                placeholder={"0.75 (75%)"}
+                accessibleName={"Header background opacity"}
+            />
+          </UKContainer>
+          <UKContainer>
+            <UKHeading
+                className={styles.heading}
+                style={{ textAlign: "start" }}
+                text={"Header background blur (%)"}
+                level={4}
+            />
+            <UKTextInput
+                className={styles.input}
+                getValue={() => {}}
+                placeholder={"0.75 (75%)"}
+                accessibleName={"Header background blur"}
+            />
+          </UKContainer>
+        </UKFlex>
+      </UKCard>
+    </>) : (<UKFlex
+        direction={"row"}
+        className={styles.widgetsCarouselContainer}
+    >
+      <UKText text={"TODO: fix the carousel"}/>
+      <UKCarousel
+          className={styles.carousel}
+          items={[
+            {
+              element: (<div className={styles.widgetGrid}>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+              </div>), id: "page1",
+            }, {
+              element: (<div className={styles.widgetGrid}>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+              </div>), id: "page2",
+            },{
+              element: (<div className={styles.widgetGrid}>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+                <UKCard>Item</UKCard>
+              </div>), id: "page3",
+            },
+          ]}
+      />
+    </UKFlex>)}
+  </div>);
 };
 export default DashApplication;
